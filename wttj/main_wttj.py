16c2f183a907d1c312195e87f5eaf0858e76300a
MoSizzle87 <mohamed.ek.gassama@gmail.com>
2024-05-14 17:41:15 +0200

    organisation du repo

--- wttj/main_wttj.py
+++ wttj/main_wttj.py
@@ -20,10 +20,10 @@ from scrap_wttj.pagination_functions import get_total_pages, get_html
 from scrap_wttj.file_operations import save_file
 
 
-# Configuration du journal dans un fichier
-logging.basicConfig(level=logging.INFO, filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
-
-# Configuration du journal pour afficher uniquement les erreurs dans la console
+# Setup logging
+current_dir = Path(__file__).resolve().parent
+logging.basicConfig(level=logging.INFO, filename=f'{current_dir}/app.log', filemode='a',
+                    format='%(asctime)s - %(levelname)s - %(message)s')
 console = logging.StreamHandler()
 console.setLevel(logging.WARNING)
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -34,74 +34,51 @@ async def generate_job_search_url(job, page_number):
     return f"https://www.welcometothejungle.com/fr/jobs?query={job.replace(' ', '%20')}&page={page_number}&aroundQuery=worldwide"
 
 
+async def scrape_job_offers(browser, job, page_number):
+    job_search_url = await generate_job_search_url(job, page_number)
+    page = await browser.new_page()
+    job_links = await extract_links(page, job_search_url, JOB_LINK_SELECTOR)
+    job_offers = []
+
+    for link in job_links:
+        complete_url = f'{RACINE_URL}{link}'
+        html = await get_html(complete_url)
+        if html:
+            job_offer = {
+                'source': "welcometothejungle",
+                **await get_contract_elements(html, CONTRACT_INFO_SELECTOR, CONTRACT_SELECTORS),
+                'company_data': await get_company_elements(html, COMPANY_INFO_SELECTOR, COMPANY_SELECTORS),
+                'skills': await get_job_skills(html, JOB_DESCRIPTION_SELECTOR, SKILLS_DICT),
+                'description': await get_raw_description(html, JOB_DESCRIPTION_SELECTOR)
+            }
+            job_offers.append(job_offer)
+            print(job_offer)
+
+    await page.close()  # Fermer la page après avoir terminé
+    return job_offers
+
 async def main():
-
-    all_job_offers = []
-    for job in JOBS:
-        baseurl = await generate_job_search_url(job, page_number=1)
-        logging.warning(f"Collecting data for {job} position")
-
-        try:
-            total_pages = await get_total_pages(baseurl, TOTAL_PAGE_SELECTOR)
-            logging.info(f"Number of pages : {total_pages}")
-
-            async with async_playwright() as p:
-                browser = await p.chromium.launch()
-
-                for page_number in range(1, total_pages + 1):
-                    logging.warning(f"Collecting data for page {page_number}/{total_pages} for {job} position")
-                    job_search_url = await generate_job_search_url(job, page_number)
-                    page = await browser.new_page()
-                    job_links = await extract_links(page, job_search_url, JOB_LINK_SELECTOR)
-
-                    for i, link in enumerate(job_links, start=1):
-                        logging.info(f'Scraping page {page_number} offer {i} for {job} position')
-                        complete_url = f'{RACINE_URL}{link}'
-                        html = await get_html(complete_url)
-                        if html:
-                            # Use a dictionary for each job offer
-                            job_offer = {}
-
-                            # Add contract_data, company_data, skill_categories, raw_description as sub-dictionaries
-                            job_offer['source'] = "welcometothejungle"
-
-                            contract_data = await get_contract_elements(html, CONTRACT_INFO_SELECTOR,
-                                                                                     CONTRACT_SELECTORS)
-                            for key, value in contract_data.items():
-                                job_offer[key] = value
-
-                            job_offer['company_data'] = await get_company_elements(html, COMPANY_INFO_SELECTOR,
-                                                                                   COMPANY_SELECTORS)
-
-                            job_offer['skills'] = await get_job_skills(html, JOB_DESCRIPTION_SELECTOR,
-                                                                                 SKILLS_DICT)
-
-                            job_offer['link'] = complete_url
-
-                            job_offer['description'] = await get_raw_description(html, JOB_DESCRIPTION_SELECTOR)
-
-                            all_job_offers.append(job_offer)
-
-        except Exception as e:
-            logging.error(f'Unexpected error : {e}')
-
-    # Remove duplicate dictionaries from all_job_offers
     unique_job_offers = []
     seen_job_offers = set()
-    for job_offer in all_job_offers:
-        job_offer_tuple = tuple(sorted(job_offer.items()))  # Convert the dictionary to a tuple of sorted items
-        if job_offer_tuple not in seen_job_offers:
-            unique_job_offers.append(job_offer)
-            seen_job_offers.add(job_offer_tuple)
 
-    all_job_offers = unique_job_offers  # Replace all_job_offers with the unique list
+    async with async_playwright() as p:
+        browser = await p.chromium.launch()
 
-    # Get the week number
+        for job in JOBS:
+            baseurl = await generate_job_search_url(job, 1)
+            total_pages = await get_total_pages(baseurl, TOTAL_PAGE_SELECTOR)
+            for page_number in range(1, total_pages + 1):
+                job_offers = await scrape_job_offers(browser, job, page_number)
+                for job_offer in job_offers:
+                    job_offer_tuple = tuple(sorted(job_offer.items()))
+                    if job_offer_tuple not in seen_job_offers:
+                        unique_job_offers.append(job_offer)
+                        seen_job_offers.add(job_offer_tuple)
+
+        await browser.close()  # Fermer le navigateur après avoir terminé
+
     week_number = datetime.datetime.now().isocalendar()[1]
-    # Save the output in a json file with the week number in the name
-    save_file(all_job_offers, f'wttj_database_{week_number}.json')
-
+    save_file(unique_job_offers, f'wttj_database_{week_number}.json')
 
 if __name__ == "__main__":
     asyncio.run(main())
-    sys.exit()
